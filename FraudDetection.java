a54fcb0782585744a77d5cadb4be3efbfb04b54c
pterodactylogan <loganswan37@gmail.com>
2018-04-04 12:54:34 -0400

    Set up data structures

--- FraudDetection.java
+++ FraudDetection.java
@@ -1,6 +1,9 @@
 /*
  * Coding Challenge 6
  * Authors: Logan Swanson, Sherri Lin
+ * This program reads in the specified csv file of credit card transactions
+ * and creates a new csv containing all transactions flagged for possible fraud
+ * see accompanying README.txt for details
  */
 
 
@@ -14,21 +17,68 @@ import java.nio.charset.StandardCharsets;
 public class FraudDetection {
 
 	public static void main(String[] args) {
+		//REPLACE W args[0]
+		//set up input file
 		File in = new File("oklahoma-pcard-FY2014.csv");
 		try{
+			//create parser to read through file
 			CSVParser parser = CSVParser.parse(in, StandardCharsets.ISO_8859_1, CSVFormat.EXCEL.withHeader());
-			File file= new File("Test.csv");
+			//set up output file, and overwrite whatever was already there
+			File file= new File("FlaggedRecords.csv");
 			file.delete();
-			BufferedWriter writer = new BufferedWriter(new FileWriter("Test.csv", true));
+			//create writer for output file, and feed it to a csvPrinter for formatting purposes
+			BufferedWriter writer = new BufferedWriter(new FileWriter("FlaggedRecords.csv", true));
 			CSVPrinter printer = new CSVPrinter(writer,CSVFormat.EXCEL.withHeader());
+			
+			//set up map to store records by person (Initial+Last: List<CSVRecord>)
+			HashMap users = new HashMap<String, ArrayList<CSVRecord>>();
+			//set up map to store records by airline
+			HashMap airlines = new HashMap<String, ArrayList<CSVRecord>>();
+			
+			//loop through each record
 			for(CSVRecord record: parser) {
-				//STORE RECORD IN AN INTELLIGENT DATASET
-				String amount = record.get("Amount");
-				if(Float.parseFloat(amount)>=50000) {
-					//System.out.println(record);
+				//put this record in the map 
+				String userID = record.get("Cardholder First Initial")+". "+record.get("Cardholder Last Name");
+				if(users.containsKey(userID)) {
+					ArrayList<CSVRecord> records = (ArrayList<CSVRecord>) users.get(userID);
+					records.add(record);
+					users.put(userID, records);
+				}else {
+					ArrayList<CSVRecord> records=new ArrayList();
+					records.add(record);
+					users.put(userID,records);
+				}
+				
+				//if this transaction was for air travel, add to airlines map
+				if(record.get("Description").equals("AIR TRAVEL")) {
+					String airline = record.get("Merchant Category Code (MCC)");
+					if(airlines.containsKey(airline)) {
+						ArrayList<CSVRecord> records = (ArrayList<CSVRecord>) users.get(userID);
+						records.add(record);
+						airlines.put(airline,records);
+					}else {
+						//System.out.println("adding "+airline);
+						ArrayList<CSVRecord> records = new ArrayList();
+						records.add(record);
+						airlines.put(airline,records);
+					}
+				}
+				
+				//check single-record flags
+				String amountstr = record.get("Amount");
+				Float amount = Float.parseFloat(amountstr);
+				if(amount>=50000 || amount%100==0) {
 					printer.printRecord(record);
 				}
+				String vendor=record.get("Vendor");
+				if(vendor.contains("PAWN ") || vendor.contains("RESORT")|| vendor.contains("CASINO")) {
+					printer.printRecord(record);
+				}
+				
+				
 			}
+			printer.close();
+			writer.close();
 			}catch(IOException e) {
 				e.printStackTrace();
 			}
