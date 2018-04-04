252a5a70fd5896a89b5b7ba7c27acb0813b78b5c
pterodactylogan <loganswan37@gmail.com>
2018-04-03 22:55:32 -0400

    creates csv for flagged records

--- FraudDetection.java
+++ FraudDetection.java
@@ -1,7 +1,14 @@
+/*
+ * Coding Challenge 6
+ * Authors: Logan Swanson, Sherri Lin
+ */
+
+
 import java.util.*;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 public class FraudDetection {
@@ -10,11 +17,16 @@ public class FraudDetection {
 		File in = new File("oklahoma-pcard-FY2014.csv");
 		try{
 			CSVParser parser = CSVParser.parse(in, StandardCharsets.ISO_8859_1, CSVFormat.EXCEL.withHeader());
-
+			File file= new File("Test.csv");
+			file.delete();
+			BufferedWriter writer = new BufferedWriter(new FileWriter("Test.csv", true));
+			CSVPrinter printer = new CSVPrinter(writer,CSVFormat.EXCEL.withHeader());
 			for(CSVRecord record: parser) {
+				//STORE RECORD IN AN INTELLIGENT DATASET
 				String amount = record.get("Amount");
 				if(Float.parseFloat(amount)>=50000) {
-					System.out.println(record.toString());
+					//System.out.println(record);
+					printer.printRecord(record);
 				}
 			}
 			}catch(IOException e) {
